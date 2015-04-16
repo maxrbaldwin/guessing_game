33e0ae0d892658b8544af915d26001cb52c1b28a
maxrbaldwin <maxrbaldwin@outlook.com>
2015-04-16 19:57:56 -0400

    Added functions for giving a hint as well as added modal show and hide

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ var guess = function() {
 
     var $remaining = $('span.remaining'),
         $user_number = $('input.game-input'),
-        $temp_img = $('div.current_status img');
+        $current_status = $('div.current-status h2'),
+        $guess_list = $('ul.guess-list'),
+        $hint = $('div.hint'),
+        $loser_modal = $('#loser-modal'),
+        $winner_modal = $('#winner-modal'),
+        $first_time_modal = $('#first-time-modal'),
+        $modal = $('.modal');
 
     var remaing_number = 5;
 
     var random,
         temp;
-
-    var temp_images = {
-        hot: ['http://i.giphy.com/PCxzeSqlWt05O.gif', 'http://i.giphy.com/CnPj5PVSJHVzG.gif', 'http://i.giphy.com/AXk4V1KZBdCO4.gif'],
-        cold: ['http://i.giphy.com/10PrLKmZzVcK9a.gif', 'http://i.giphy.com/nUA07lc5wis5a.gif', 'http://i.giphy.com/PEKztVNo5AMyk.gif']
-    }
 
     var _load = {
         init: function() {
@@ -34,22 +35,25 @@ var guess = function() {
             var number_guess = $user_number.val(),
                 minus_one = $remaining.text();
 
-            _check.hot_or_cold(number_guess);
+            _check.hot_or_cold(parseInt(number_guess));
             _check.check_int(number_guess, minus_one);
         },
         check_int: function(user_guess, guess_left) {
-            if(user_guess % 1 === 0) {
+            if($.isNumeric(user_guess)) {
                 _check.minus_one(guess_left);
+                _set.guess_list(user_guess);
             } else {
-                check.minus_one(guess_left);
+                _check.minus_one(guess_left);
+                _set.guess_list(0);
                 _set.not_a_number();
             }
         },
         minus_one: function(left) {
             var left_over = parseInt(left);
 
-            if(left_over === 0) {
+            if(left_over === 1) {
                 _set.you_lose();
+                _load.number_remaining(left_over - 1);
             } else {
                 _load.number_remaining(left_over - 1);
             }
@@ -67,28 +71,66 @@ var guess = function() {
 
     var _set = {
         you_lose: function() {
-            console.log('loser');
+            $loser_modal.modal('show');
         },
         not_a_number: function() {
-
+            console.log('that is not a number');
         },
         play_again: function() {
             _load.init();
+            _set.reset();
+            $modal.modal('hide');
         },
         tempature: function(temp) {
             var image_number = _set.random_temp();
 
             if(temp === 'hot') {
-                $temp_img.attr('src', temp_images.hot[image_number]);
+                $current_status.text('HOT!');
             } else if(temp === 'cold') {
-                $temp_img.attr('src', temp_images.cold[image_number]);
+                $current_status.text('COLD!');
             }
         },
         winner: function() {
-
+            if($guess_list.children('li').length === 0) {
+                $first_time_modal.modal('show');
+            } else {
+                $winner_modal.modal('show');
+            }
         },
         random_temp: function() {
-            return Math.random() * (0 - 2) + 0;
+            return Math.floor(Math.random() * 3) + 1
+        },
+        guess_list: function(user_guess) {
+            $guess_list.append('<li class="guess">' + user_guess + '</li>');
+        },
+        reset: function() {
+            $user_number.val('');
+            $guess_list.html('');
+            $hint.html('');
+            $current_status.text('');
+        }
+    }
+
+    var _hint = {
+        give: function() {
+            var between = _hint.find_between();
+
+            _check.minus_one(_store.remainder);
+
+            $hint.children('h2').text(between);
+        },
+        find_between: function() {
+            var low = _store.random_num - 10,
+                high = _store.random_num + 10;
+
+            if(low <= 0) {
+                low = 0
+            }
+            if(high >= 100) {
+                high = 100;
+            }
+
+            return 'The number is between ' + low + ' and ' + high;
         }
     }
 
@@ -103,6 +145,7 @@ var guess = function() {
         load: _load.init,
         store: _store,
         set: _set,
-        check: _check.register
+        check: _check.register,
+        hint: _hint
     }
 }();
